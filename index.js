8fbf567e21e3353d81b488136a99306870a81979
Qin Guan <qinguan20040914@gmail.com>
2019-03-09 01:01:41 +0800

    fuck

--- index.js
+++ index.js
@@ -2,17 +2,48 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const firebase = require("firebase");
+const atob = require("atob");
+const validator = require("validator");
+
+var config = {
+  apiKey: "AIzaSyAcYSb9fvk2vUiRhavsKlrcrpnyFKtzXTQ",
+  authDomain: "master-eb220.firebaseapp.com",
+  databaseURL: "https://master-eb220.firebaseio.com",
+  projectId: "master-eb220",
+  storageBucket: "master-eb220.appspot.com",
+  messagingSenderId: "1086655103908"
+};
+firebase.initializeApp(config);
 
 app.use(cors());
 app.use(bodyParser());
 
-app.get("/", (req, res) => {
-  res.send("tf");
-});
-
-app.post("/post", (req, res) => {
-  res.end("posted");
-  console.log(req.body);
+app.post("/llcllogin", (req, res) => {
+  const email = atob(req.body.e);
+  const pass = atob(req.body.p);
+  if (!validator.isEmail(email)) {
+    res.send({ error: true, message: "Email is not valid." }).end();
+    return;
+  }
+  if (!validator.isLength(pass, { min: 6, max: undefined })) {
+    res
+      .send({
+        error: true,
+        message: "Password has to be 6 characters or more"
+      })
+      .end();
+    return;
+  }
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, pass)
+    .then(x => {
+      res.send(x).end();
+    })
+    .catch(error => {
+      res.send({ error: true, ...error }).end();
+    });
 });
 
 app.listen(8080);
