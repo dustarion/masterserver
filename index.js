9590ade5c2fa740cc3eb1642df8e50737d33a52f
Qin Guan <qinguan20040914@gmail.com>
2019-03-10 21:25:58 +0800

    auth

--- index.js
+++ index.js
@@ -32,21 +32,72 @@ app.post("/localSignIn", (req, res) => {
     return res.send({ error: true, msg: "Missing fields" }).end();
   }
   const uid = req.body.uid;
-  const email = req.body.email;
+  const email = req.body.e;
   const ref = db.collection("users").doc(uid);
   ref.get().then(doc => {
     if (!doc.exists) {
-      return res.send({ error: true, msg: "User does not exist" });
+      return res.send({ error: true, msg: "User does not exist" }).end();
     }
-    const user = {
+    const jwtUser = {
       uid,
       email
     };
-    jwt.sign(user, secret, (err, token) => {
+    jwt.sign(jwtUser, secret, (err, token) => {
       if (err) {
-        return res.send({ error: true, msg: "a server error occurred" });
+        return res.send({ error: true, msg: "a server error occurred" }).end();
       }
-      res.send(token).end();
+      res.send({ token, pfp: doc.data().pfp }).end();
+    });
+  });
+});
+
+app.post("/redirectSignUp", (req, res) => {
+  if (!req.body.uid) {
+    return res.send({ error: true, msg: "Missing fields" }).end();
+  }
+  if (!req.body.e) {
+    return res.send({ error: true, msg: "Missing fields" }).end();
+  }
+  if (!req.body.n) {
+    return res.send({ error: true, msg: "Missing fields" }).end();
+  }
+  const uid = req.body.uid;
+  const email = req.body.e;
+  const name = req.body.n;
+  const ref = db.collection("users").doc(uid);
+  ref.get().then(doc => {
+    if (!doc.exists) {
+      const user = {
+        email,
+        folders: [],
+        name,
+        pfp: "/static/fadedBar.png",
+        sets: []
+      };
+      ref.set(user).then(() => {
+        const jwtUser = {
+          uid,
+          email
+        };
+        jwt.sign(jwtUser, secret, (err, token) => {
+          if (err) {
+            return res
+              .send({ error: true, msg: "a server error occurred" })
+              .end();
+          }
+          res.send({ token, pfp: user.pfp }).end();
+        });
+      });
+    }
+    const jwtUser = {
+      uid,
+      email
+    };
+    jwt.sign(jwtUser, secret, (err, token) => {
+      if (err) {
+        return res.send({ error: true, msg: "a server error occurred" }).end();
+      }
+      res.send({ token, pfp: doc.data().pfp }).end();
     });
   });
 });
